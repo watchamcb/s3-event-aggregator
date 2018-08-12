b76b776328a01f99025fe55d88609ab6a5a68645
Craig Watcham <craig.watcham@gmail.com>
2018-08-12 17:45:24 +0100

    Use logger to change runtime log levels

--- src/s3_aggregator.py
+++ src/s3_aggregator.py
@@ -4,22 +4,24 @@ import os
 import sys
 from datetime import datetime
 from dateutil.parser import parse
+import logging
 
 sqs = boto3.client('sqs')
 dynamodb = boto3.client('dynamodb')
+log = logging.getLogger('S3EventAggregator')
+log.setLevel(os.environ['LOG_LEVEL'])
+queue = os.environ['QUEUE_URL']
 refresh_delay = int(os.environ['REFRESH_DELAY_SECONDS'])
 
-def handle_exception(*logs):
-    exctype, value = sys.exc_info()[:2]
-    print(*logs, exctype, value)
-
 def update_dynamo(bucket, timestamp):
+    log.debug('Performing DynamoDB update for bucket %s and timestamp %d',
+        bucket, timestamp)
     try:
         dynamodb.update_item(TableName='S3EventAggregator', 
             Key={ 'BucketName' : { 'S': bucket } },
             ExpressionAttributeNames={ '#t': 'timestamp' },
             ExpressionAttributeValues={
-                ':t' : { 'N': str(timestamp + refresh_delay) },
+                ':t' : { 'N': str(timestamp + (refresh_delay * 1000)) },
                 ':x' : { 'N': str(timestamp) }
             }, 
             UpdateExpression = 'SET #t = :t',
@@ -29,19 +31,18 @@ def update_dynamo(bucket, timestamp):
         return True
     except botocore.exceptions.ClientError as e:
         if (e.response['Error']['Code'] == 'ConditionalCheckFailedException'):
-            print('Refresh for bucket:', bucket, 
-                'within refresh window, skipping. S3 Event timestamp:', 
-                timestamp)
+            log.info('Refresh for bucket: %s within refresh window, '
+                'skipping. S3 Event timestamp: %d', bucket, timestamp)
         else:
-            print('ClientError processing DynamoDB update for bucket:', bucket, 
-                'ignoring event, exception:', e)
+            log.error('ClientError processing DynamoDB update for bucket: %s'
+                ' ignoring event, exception: %s', bucket, e)
     except:
-        handle_exception('Error processing DynamoDB update for bucket:', bucket, 
-            'ignoring event, exception:')
+        exctype, value = sys.exc_info()[:2]
+        log.error('Error processing DynamoDB update for bucket: %s ' 
+            'ignoring event, exception: %s %s', bucket, exctype, value)
     return False
 
 def send_refresh(bucket, timestamp):
-    queue = os.environ['QUEUE_URL']
     try:
         sqs.send_message(
             QueueUrl=queue,
@@ -58,13 +59,16 @@ def send_refresh(bucket, timestamp):
             },
             MessageBody='{}')
     except:
-        handle_exception('Error sending refresh request for bucket:', bucket, 
-            'ignoring event, exception:')
+        exctype, value = sys.exc_info()[:2]
+        log.error('Error sending refresh request for bucket: %s '
+                'ignoring, exception: %s %s', bucket, exctype, value)
 
 def handle_s3_event(message):
-    timestamp = parse(message['eventTime']).timestamp()
+    timestamp = parse(message['eventTime']).timestamp() * 1000
     bucket = message['s3']['bucket']['name']
     if update_dynamo(bucket, timestamp):
+        log.info('Sending refresh request for bucket: %s, timestamp: %d', 
+            bucket, timestamp)
         send_refresh(bucket, timestamp)
 
 def is_storage_gateway_event(message):
@@ -79,14 +83,16 @@ def invalid_event(event):
     return False
 
 def lambda_handler(event, context):
+    log.debug('Event received: %s', event)
     if invalid_event(event):
-        print('Ignoring event with unexpected format:', event)
+        log.warn('Ignoring event with unexpected format: %s', event)
         return
     for message in event['Records']:
         if is_storage_gateway_event(message):
-            print('Ignoring Storage Gateway operation:', message)
+            log.info('Ignoring Storage Gateway operation: %s', message)
             continue
         if 's3' not in message:
-            print('Ignoring message missing s3 section:', message)
+            log.warn('Ignoring message missing s3 section: %s', message)
             continue
         handle_s3_event(message)
+
